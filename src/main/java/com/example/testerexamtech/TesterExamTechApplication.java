b6ef9368b8c8302817c80609fad2c79b48bb8309
DennisLojaCastillo <dennislojacastilllo@gmail.com>
2024-01-02 21:05:09 +0100

    Initial commit - Packages added

---
+++ src/main/java/com/example/testerexamtech/TesterExamTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.testerexamtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesterExamTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesterExamTechApplication.class, args);
+    }
+
+}
